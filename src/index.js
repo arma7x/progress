7bb0a8d003692d3128fdbf50fe7185ee67574d0b
arma7x <arma7x@live.com>
2018-12-03 16:47:28 +0800

    new build

--- src/index.js
+++ src/index.js
@@ -13,15 +13,12 @@ function init() {
 init();
 
 if (process.env.NODE_ENV === 'production') {
-	// cache all assets if browser supports serviceworker
 	if ('serviceWorker' in navigator && location.protocol === 'https:') {
 		navigator.serviceWorker.register('/sw.js');
 	}
 
 } else {
-	// use preact's devtools
 	require('preact/devtools');
-	// listen for HMR
 	if (module.hot) {
 		module.hot.accept('./views', init);
 	}

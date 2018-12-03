441471c5a829b8fd29a1dc3a42754af538ef41c5
arma7x <arma7x@live.com>
2018-12-03 21:32:05 +0800

    new build

--- src/views/pages/errors/404.js
+++ src/views/pages/errors/404.js
@@ -5,8 +5,10 @@ export default function (props) {
 	return (
 		<div className="page page__404">
 			<Card>
-				<h1>404 Page</h1>
-				<p>Looks like you were given a bad link ;-)</p>
+				<div class="animated heartBeat">
+					<h1>404 Page</h1>
+					<p>Looks like you were given a bad link ;-)</p>
+				</div>
 			</Card>
 		</div>
 	);
